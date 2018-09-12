35c9240293c35d66265fbf9acd8ef10c49c3c9b4
Elukwal <elukwal3@gmail.com>
2018-09-12 16:34:40 +0300

    add changes made on the appcomponent

---
+++ Goals/src/app/app.component.ts
@@ -0,0 +1,15 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+    goals: string[];
+
+  constructor(){
+
+    this.goals = ["Watch Finding Nemo","Buy Cookies","Get new phone case"];
+  }
+}
